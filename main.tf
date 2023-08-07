7a0512a6d1bb9b676553da047e84e777d561af20
L-theRing96 <shenhong5796@gmail.com>
2023-08-07 13:25:12 +0800

    error

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "cloudflare_zone" "theorangeonenet" {
 resource "cloudflare_record" "theorangeonenet_git" {
   zone_id = cloudflare_zone.theorangeonenet.id
   name    = "git"
-  value   = 8.8.8.8
+  value   = "theorange.com"
   type    = "A"
   ttl     = 1
 }
