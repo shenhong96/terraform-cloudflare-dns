113e671b8227bdf8b6ac94957a61e95558a49161
L-theRing96 <shenhong5796@gmail.com>
2023-08-27 11:34:52 +0800

    initialize module

--- main.tf
+++ main.tf
@@ -1,11 +1,47 @@
-resource "cloudflare_zone" "theorangeonenet" {
-  zone = "theorangeone.net"
+locals {
+  common_config = {
+    zone_id = var.zone_id
+    type    = "CNAME"
+  }
+
+  records = flatten([
+    for target_host, list_of_records in var.map_of_records : [
+      for rec in list_of_records : merge(
+        {
+          ttl     = var.default_ttl
+          value   = target_host
+        }, rec
+      )
+    ]
+  ])
 }
 
-resource "cloudflare_record" "theorangeonenet_git" {
-  zone_id = cloudflare_zone.theorangeonenet.id
-  name    = "git"
-  value   = "theorange.com"
-  type    = "A"
-  ttl     = 1
+variable "zone_id" {
+  description = "Zone ID for the Cloudflare domain"
+  type        = string
+}
+
+variable "default_ttl" {
+  description = "Default Time to Live for DNS records"
+  type        = number
+  default     = 300
+}
+
+variable "map_of_records" {
+  description = "Map of values to list of records with that value"
+  type = map(list(object({
+    name    = string
+    proxied = bool
+    ttl     = optional(number)
+  })))
+}
+
+resource "cloudflare_record" "this" {
+  for_each = { for rec in local.records : "${rec.name}-${rec.value}" => rec }
+  name      = each.value.name
+  value     = each.value.value
+  type      = local.common_config.type
+  zone_id   = local.common_config.zone_id
+  ttl       = each.value.ttl
+  proxied   = each.value.proxied
 }
